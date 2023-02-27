25ed36f2de8a0466915c5e912992fe2b62ef390c
Arshad Ahmed <ahmed.a0611@gmail.com>
2023-02-27 08:50:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -29,4 +29,12 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
   })
 }
 
-
+function Welcome(name) {
+  var greetingInfo = function (message) {
+    console.log(message + " " + name);
+  };
+  return greetingInfo;
+}
+var myFunction = Welcome("John");
+myFunction("Welcome "); //Output: Welcome John
+myFunction("Hello Mr."); //output: Hello Mr.John
